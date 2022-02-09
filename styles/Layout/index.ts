e316b8f22a082aa9426f00f5006cb1ca20d08b7f
Saveliy <ssandry.github@gmail.com>
2022-02-09 20:46:28 +0300

    Fix tsconfig | Fix basic layout styles

--- styles/Layout/index.ts
+++ styles/Layout/index.ts
@@ -3,5 +3,8 @@ import { StyleSheet } from "react-native";
 export default StyleSheet.create({
   Layout: {
     backgroundColor: "#161622",
+    flex: 1,
+    paddingHorizontal: 31,
+    paddingTop: 43,
   },
 });
