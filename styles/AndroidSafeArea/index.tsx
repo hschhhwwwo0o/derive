408341083bda6f8869d07741cdd06c600d55f76e
Saveliy <ssandry.github@gmail.com>
2022-02-12 21:39:04 +0300

    Remove useless code

--- styles/AndroidSafeArea/index.tsx
+++ styles/AndroidSafeArea/index.tsx
@@ -1,9 +1,8 @@
-import { StyleSheet, Platform, StatusBar } from "react-native";
+import { StyleSheet } from "react-native";
 
 export default StyleSheet.create({
   AndroidSafeArea: {
     flex: 1,
     backgroundColor: "#161622",
-    // paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
   },
 });
