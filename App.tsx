e49f15d7f9e230de6bc30b8a348b4c24e0474599
Saveliy <ssandry.github@gmail.com>
2022-02-12 21:40:58 +0300

    Fix font loaded const name

--- App.tsx
+++ App.tsx
@@ -2,13 +2,13 @@ import { useFonts } from "expo-font";
 import Router from "router";
 
 function App() {
-  const [loaded] = useFonts({
+  const [loadedFonts] = useFonts({
     "Lato-Black": require("./assets/fonts/Lato-Black.ttf"),
     "Lato-Bold": require("./assets/fonts/Lato-Bold.ttf"),
     "Lato-Regular": require("./assets/fonts/Lato-Regular.ttf"),
   });
 
-  if (!loaded) {
+  if (!loadedFonts) {
     return null;
   }
 
