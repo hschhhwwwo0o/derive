6fa4ce02c9dc82be128b800b8ebd62093133855a
Saveliy <ssandry.github@gmail.com>
2022-02-13 14:17:39 +0300

    Transaction page

--- screens/Transactions/index.tsx
+++ screens/Transactions/index.tsx
@@ -3,13 +3,33 @@ import { StyleSheet, View } from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import TheLayout from "layouts";
 import TopPanel from "components/UI/TopPanel";
+import Label from "components/UI/Label";
+import Transaction from "components/Custom/Transaction";
 
 const TransactionsScreen: FunctionComponent<IScreen> = ({ navigation }) => {
   return (
     <>
       <TheLayout>
         <TopPanel withBack navigation={navigation} />
-        <View style={styles.body}></View>
+        <View style={styles.body}>
+          <Label>All Transactions</Label>
+          <View style={styles.data}>
+            <Transaction />
+            <Transaction />
+            <Transaction />
+            <Transaction />
+            <Transaction />
+            <Transaction />
+            <Transaction />
+            <Transaction />
+            <Transaction />
+            <Transaction />
+            <Transaction />
+            <Transaction />
+            <Transaction />
+            <Transaction />
+          </View>
+        </View>
       </TheLayout>
     </>
   );
@@ -17,7 +37,11 @@ const TransactionsScreen: FunctionComponent<IScreen> = ({ navigation }) => {
 
 const styles = StyleSheet.create({
   body: {
-    marginTop: 46,
+    marginTop: 39,
+    paddingBottom: 39,
+  },
+  data: {
+    marginTop: 11,
   },
 });
 
