66fbdec649f2ae9c6bcf783daa7b13871e83e082
Saveliy <ssandry.github@gmail.com>
2022-02-13 12:29:48 +0300

    Add multiline input | Add constants

---
+++ components/UI/MultilineInput/index.tsx
@@ -0,0 +1,43 @@
+import React, { Dispatch, FunctionComponent, SetStateAction } from "react";
+import { StyleSheet, TextInput } from "react-native";
+
+interface IMultilineInput {
+  state: string;
+  setState: Dispatch<SetStateAction<string>>;
+  placeholder: string;
+}
+
+const MultilineInput: FunctionComponent<IMultilineInput> = ({
+  placeholder = "Enter a value...",
+  state = "",
+  setState = () => {},
+}) => {
+  return (
+    <>
+      <TextInput
+        multiline={true}
+        numberOfLines={4}
+        value={state}
+        onChangeText={newText => setState(newText)}
+        keyboardAppearance="dark"
+        style={styles.input}
+        placeholder={placeholder}
+        placeholderTextColor="#595968"
+      />
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  input: {
+    fontSize: 14,
+    fontFamily: "Lato-Bold",
+    color: "#9393A2",
+    paddingVertical: 18,
+    paddingHorizontal: 18,
+    backgroundColor: "#1E1E2D",
+    borderRadius: 20,
+  },
+});
+
+export default MultilineInput;
---
+++ constants/index.ts
@@ -0,0 +1,18 @@
+const AppConstants = {
+  BackgroundColor: "#161622",
+  BackgroundSecondColor: "#1E1E2D",
+  PaddingHorizontal: 31,
+  LayoutPaddingTop: 43,
+  FontExtra: "Lato-Black",
+  FontBold: "Lato-Bold",
+  FontRegular: "Lato-Regular",
+  CardSkins: [
+    { colors: ["#FFDEC2", "#FACCD1", "#C7F0FC"], id: 0 },
+    { colors: ["#F5BCE9", "#C7F0FC"], id: 1 },
+    { colors: ["#CCF0FA", "#E6F5BC"], id: 2 },
+    { colors: ["#FFDEC2", "#C7F0FC"], id: 3 },
+    { colors: ["#C7F0FC", "#FACCD1", "#FFDEC2"], id: 4 },
+  ],
+};
+
+export default AppConstants;
