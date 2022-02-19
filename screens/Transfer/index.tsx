76f0f871808fc8a7c940099d597dcc80fc0f72ff
Saveliy <ssandry.github@gmail.com>
2022-02-19 12:54:58 +0300

    Initialize new screens

---
+++ screens/Transfer/index.tsx
@@ -0,0 +1,27 @@
+import React, { FunctionComponent } from "react";
+import { StyleSheet, View } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import TheLayout from "layouts";
+import TopPanel from "components/UI/TopPanel";
+import Label from "components/UI/Label";
+import Transaction from "components/Custom/Transaction";
+
+const TransferScreen: FunctionComponent<IScreen> = ({ navigation }) => {
+  return (
+    <>
+      <TheLayout>
+        <TopPanel withBack navigation={navigation} />
+        <View style={styles.body}></View>
+      </TheLayout>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  body: {
+    marginTop: 39,
+    paddingBottom: 39,
+  },
+});
+
+export default TransferScreen;
