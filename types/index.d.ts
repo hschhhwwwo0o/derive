da0b46e08b6b298efe7f670970bb10206973fa8e
Saveliy <ssandry.github@gmail.com>
2022-02-20 16:18:02 +0300

    Add route interface

--- types/index.d.ts
+++ types/index.d.ts
@@ -3,6 +3,14 @@ interface INavigation {
   navigate: (path: string, params?: any) => any;
   goBack: () => any;
 }
+
+interface IRoute {
+  key: string;
+  path: string;
+  name: string;
+  params: any;
+}
 interface IScreen {
   navigation: INavigation;
+  route: IRoute;
 }
